b85eab0bc13d32da814c09a85a0f2230fa3f1e03
unknown <jainsakshi9301@gmail.com>
2022-05-27 15:20:38 +0530

    add author

--- src/components/News.js
+++ src/components/News.js
@@ -75,7 +75,7 @@ export class News extends Component {
           <div className="row">
               {!this.state.loading && this.state.articles.map((element) => {
                   return <div key={element.url} className="col-md-4">
-                  <NewsItem  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                  <NewsItem author={element.author} date={element.publishedAt} title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url} />
                 </div>
               })}
              
