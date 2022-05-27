3d75a13c11dcb4b3392daccc6700bcfc52636bb5
unknown <jainsakshi9301@gmail.com>
2022-05-27 16:29:50 +0530

    add .env.local file

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import { Routes, Route } from "react-router-dom"
 
 export default class App extends Component {
   pageSize = 5;
+  apiKey = process.env.REACT_APP_NEWS_API
+
   render() {
     return (
       <div>
         <Navbar />
         <Routes>
-            <Route exact path="/" element={<News pageSize={this.pageSize} key="general" country="in" category="general" />}></Route>
-            <Route exact path="/science" element={<News pageSize={this.pageSize} key="science" country="in" category="science" />}></Route>
-            <Route exact path="/entertainment" element={<News pageSize={this.pageSize}  key="entertainment" country="in" category="entertainment" />}></Route>
-            <Route exact path="/sports" element={<News pageSize={this.pageSize}  key="sports" country="in" category="sports" />}></Route>
-            <Route exact path="/technology" element={<News pageSize={this.pageSize} key="technology" country="in" category="technology" />}></Route>
+            <Route exact path="/" element={<News pageSize={this.pageSize} apiKey={this.apiKey}  key="general" country="in" category="general" />}></Route>
+            <Route exact path="/science" element={<News pageSize={this.pageSize} apiKey={this.apiKey}  key="science" country="in" category="science" />}></Route>
+            <Route exact path="/entertainment" element={<News pageSize={this.pageSize} apiKey={this.apiKey}   key="entertainment" country="in" category="entertainment" />}></Route>
+            <Route exact path="/sports" element={<News pageSize={this.pageSize} apiKey={this.apiKey}   key="sports" country="in" category="sports" />}></Route>
+            <Route exact path="/technology" element={<News pageSize={this.pageSize} apiKey={this.apiKey}  key="technology" country="in" category="technology" />}></Route>
         </Routes>
       </div>
     )
--- src/components/News.js
+++ src/components/News.js
@@ -33,7 +33,7 @@ export class News extends Component {
   }
 
   async updateNews(){
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d16921fbf66045e6862f5f21216d73dc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
